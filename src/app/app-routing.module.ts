73ba4a9b600b9ed7c3672f3e7409617185770fc3
9Lukas5 <9Lukas5@users.noreply.github.com>
2017-12-20 16:58:11 +0100

    #17 Routes: add imports of all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HouseholdComponent } from './household/household.component'
+import { HouseholdArticleManagementComponent } from './household-article-management/household-article-management.component';
+import { HouseholdSettingsComponent } from './household-settings/household-settings.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes =
+[
+
+];
+
+@NgModule
+({
+    imports:
+    [
+        RouterModule.forRoot(routes)
+    ],
+
+    exports:
+    [
+        RouterModule
+    ]
 })
 export class AppRoutingModule { }
