51dc3934f615d386bf683b3dc79c6fcdd71ae78f
9Lukas5 <9Lukas5@users.noreply.github.com>
2017-12-28 20:53:19 +0100

    #17 Createing Http-Intercepter
    provide Http methods with a global handling for
    expired Authentications (which end in 403 status codes)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HttpInterceptorService } from './http-interceptor.service';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -12,27 +15,45 @@ import { HouseholdComponent } from './household/household.component';
 import { HouseholdSettingsComponent } from './household-settings/household-settings.component';
 import { HouseholdArticleManagementComponent } from './household-article-management/household-article-management.component';
 import { PurchaseComponent } from './purchase/purchase.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    ProfileComponent,
-    HouseholdComponent,
-    HouseholdSettingsComponent,
-    HouseholdArticleManagementComponent,
-    PurchaseComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule
+({
+    declarations:
+    [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        DashboardComponent,
+        ProfileComponent,
+        HouseholdComponent,
+        HouseholdSettingsComponent,
+        HouseholdArticleManagementComponent,
+        PurchaseComponent
+    ],
+
+    imports:
+    [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        HttpModule
+    ],
+
+    providers:
+    [
+        {
+            provide: Http,
+            useFactory:
+                (backend: XHRBackend, defaultOptions: RequestOptions, router: Router): Http =>
+                {
+                    return new HttpInterceptorService(backend, defaultOptions, router);
+                },
+            deps: [XHRBackend, RequestOptions, Router]
+        }
+    ],
+
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/http-interceptor.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import
+{
+    Http,
+    ConnectionBackend,
+    RequestOptions,
+    RequestOptionsArgs,
+    Response,
+    Headers,
+    Request
+} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { environment } from '../environments/environment';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class HttpInterceptorService extends Http
+{
+    private router: Router;
+
+    constructor(backend: ConnectionBackend, defaultOptions: RequestOptions, router: Router)
+    {
+        super (backend, defaultOptions);
+        this.router = router;
+    }
+
+    public get(url: string, options?: RequestOptionsArgs): Observable<Response>
+    {
+        url = this.updateUrl(url);
+        return super.get(url, this.getRequestOptionArgs(options));
+    }
+
+    public post(url: string, body: string, options?: RequestOptionsArgs): Observable<Response>
+    {
+        url = this.updateUrl(url);
+        return super.post(url, body, this.getRequestOptionArgs(options))
+            .catch(res =>
+            {
+                if (res.status === 403)
+                {
+                    console.log("Access denied, move to login page now");
+                    this.router.navigateByUrl('/login');
+                }
+                return Observable.throw(res);
+            });
+    }
+
+    public put(url: string, body: string, options?: RequestOptionsArgs): Observable<Response>
+    {
+        url = this.updateUrl(url);
+        return super.put(url, body, this.getRequestOptionArgs(options));
+    }
+
+    public delete(url: string, options?: RequestOptionsArgs): Observable<Response>
+    {
+        url = this.updateUrl(url);
+        return super.delete(url, this.getRequestOptionArgs(options));
+    }
+
+    private updateUrl(req: string)
+    {
+        return  environment.origin + req;
+    }
+
+    private getRequestOptionArgs(options?: RequestOptionsArgs) : RequestOptionsArgs
+    {
+        if (options == null)
+        {
+            options = new RequestOptions();
+        }
+        if (options.headers == null)
+        {
+            options.headers = new Headers();
+        }
+        options.headers.append('Content-Type', 'application/json');
+
+        return options;
+    }
+
+}
