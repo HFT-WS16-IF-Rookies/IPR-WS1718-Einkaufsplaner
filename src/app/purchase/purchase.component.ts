91b1407130cbe6ce9c360dd2e1f3d1bae2ab75e5
ewolf34 <ewolf34@users.noreply.github.com>
2018-01-09 12:39:04 +0100

    #16 add rough implementation

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-purchase',
-  templateUrl: './purchase.component.html',
-  styleUrls: ['./purchase.component.css']
+@Component
+({
+    selector: 'app-purchase',
+    templateUrl: './purchase.component.html',
+    styleUrls: ['./purchase.component.css']
 })
 export class PurchaseComponent implements OnInit {
 
-  constructor() { }
+    private http: Http;
+    private router: Router;
 
-  ngOnInit() {
-  }
+    constructor(http: Http, router: Router) 
+    {
+        this.http = http;
+        this.router = router;
+    }
+
+    ngOnInit() {
+    }
 
 }
