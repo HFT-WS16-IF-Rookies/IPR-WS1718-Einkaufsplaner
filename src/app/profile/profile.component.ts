d931f70b606c31e68868a55dbf9cbd33d925dc6e
kevinkammleiter <kevinkammleiter@users.noreply.github.com>
2018-01-13 12:57:51 +0100

    #14 added some typescript code int profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -27,4 +27,70 @@ export class ProfileComponent implements OnInit
         }
     }
 
+    private submitPassword(): void
+    {
+        this.errorMsgPassword = "";
+
+        if(this.change.passowrd === "")
+        {
+            this.errorMsgPassword = "Bitte Passwort eingeben";
+        }
+
+        if(this.change.passwordConfirm === "")
+        {
+            this.errorMsgPassword = "Bitte Passwort bestätigen";
+        }
+
+        if(this.change.password != this.change.passwordConfirm)
+        {
+            this.errorMsgPassword = "Passwörte stimmen nicht überein";
+            this.change.passwordConfirm = "";
+        }
+
+        if(this.errorMsgPassword != "")
+        {
+            return;
+        }
+
+        this.http.post(/*changePassowrd.php)*/, JSON.stringify(this.change.passwordConfirm))
+            .subscribe(res =>
+            {
+                if(res.json().state === "error")
+                {
+                    this.errorMsgPassword = res.json().text;
+                }
+            });
+    }
+
+    private submitMail(): void
+    {
+        this.errorMsgMail = "";
+        if(this.change.email === "")
+        {
+            this.errorMsgMail = "Bitte Email-Adresse eingeben";
+        }
+        if(this.change.emailConfirm === "")
+        {
+            this.errorMsgMail = "Bitte Email-Adresse bestätigen";
+        }
+        if(this.change.email != this.change.emailConfirm)
+        {
+            this.errorMsgMail = "Email-Adressen stimmen nicht überein";
+            this.change.emailConfirm = "";
+        }
+        if(this.errorMsgMail != "")
+        {
+            return;
+        }
+
+        this.http.post(/*changeEMail.php*/, JSON.stringify(this.change.emailConfirm))
+        .subscribe(res =>
+        {
+            if(res.json().state === "error")
+            {
+                this.errorMsgPassword = res.json().text;
+            }
+        });
+    }
+
 }
