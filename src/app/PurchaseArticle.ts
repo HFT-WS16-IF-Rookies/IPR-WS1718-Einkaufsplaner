a5eb5db6eabe0aee9db15b674d167a2de81b509f
ewolf34 <ewolf34@users.noreply.github.com>
2018-01-15 10:48:13 +0100

    #16 add id to PurchaseArticle

--- src/app/PurchaseArticle.ts
+++ src/app/PurchaseArticle.ts
@@ -3,11 +3,13 @@ export class PurchaseArticle
     name: string;
     amount: number;
     found: number;
+    id: string;
 
-    constructor(name: string, amount: number, found: number)
+    constructor(name: string, amount: number, found: number, id: string)
     {
         this.name = name;
         this.amount = amount;
         this.found = found;
+        this.id = id;
     }
 }
