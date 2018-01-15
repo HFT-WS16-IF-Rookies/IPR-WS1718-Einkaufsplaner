94b75425ceb4c0242d0529084542dd1ee3f87382
Marco13-7 <schaefermawo@gmail.com>
2018-01-15 13:03:41 +0100

    #11 edited the app component to show the content form the json-O

--- src/app/household/household.component.ts
+++ src/app/household/household.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Http } from '@angular/http';
+import { ActiveHouseholdPurchases } from '../ActiveHouseholdPurchases';
 
 @Component
 ({
@@ -13,6 +14,7 @@ export class HouseholdComponent implements OnInit
     private http: Http;
     private router: Router;
     private route: ActivatedRoute;
+    private purchases: ActiveHouseholdPurchases[];
 
     constructor(http: Http, router: Router, route: ActivatedRoute)
     {
@@ -33,6 +35,34 @@ export class HouseholdComponent implements OnInit
         this.http.post('./getActiveHouseholdPurchases.php', JSON.stringify(data)).subscribe(res =>
         {
             console.log(res.json());
+
+            if (res.status !== 200)
+            {
+                return;
+            }
+
+            if(res.json().metaData.state === "success")
+            {
+                let temp = res.json();
+                delete temp.metaData;
+
+                this.purchases = new Array((Object.keys(temp)).length);
+                let i = 0;
+
+                for(let key in temp)
+                {
+                    this.purchases[i] = new PurchaseArticle(
+                        res.json()[key].date,
+                        +res.json()[key].store;
+                        +res.json()[key].name,
+                        +res.json()[key].amount
+                    );
+
+                    console.log(key);
+                    i++;
+
+                }
+            }
         });
 
         this.http.post('./getNeededArticles.php', JSON.stringify(data)).subscribe(res =>
