e8e64965d6b755fa7f83a048df3de30383b7d805
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-01-15 12:03:59 +0100

    #16 activate php

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -92,7 +92,10 @@ export class PurchaseComponent implements OnInit
     private closePurchase(): void
     {
         console.log(JSON.stringify(this.articles))
-        this.http.post('./closePurchase.php', JSON.stringify(this.articles))
+        this.http.post('./closePurchase.php', JSON.stringify(this.articles)).subscribe(res =>
+        {
+            // TO-DO: keine Ahnung
+        });
     }
 
 }
