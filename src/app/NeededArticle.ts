cafaeb3baf3272d15fad0f78699d5fa3701dbe9c
Marco13-7 <schaefermawo@gmail.com>
2018-01-15 18:43:49 +0100

    #11 added NeededArticles class

---
+++ src/app/NeededArticle.ts
@@ -0,0 +1,11 @@
+export class PurchaseArticle
+{
+    name: string;
+    neededAmount: number;
+
+    constructor(name: string, neededAmount: number,)
+    {
+        this.name = name;
+        this.neededAmount = neededAmount;
+    }
+}
