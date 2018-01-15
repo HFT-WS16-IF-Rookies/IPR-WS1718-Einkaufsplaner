695684367e48dd971fc41ce40ba8f279de1e84ed
Marco13-7 <schaefermawo@gmail.com>
2018-01-15 10:46:44 +0100

    # 11 added getNeededArticles.php

---
+++ php/getNeededArticles.php
@@ -0,0 +1,74 @@
+<?php
+    require './checkLogin.php';
+
+    $jsonData = json_decode(file_get_contents('php://input'), true);
+
+    if ($jsonData['ID'] === null)
+    {
+        http_response_code(400);
+        die();
+    }
+
+    $data = array();
+
+    require './dbConnection.php';
+
+    $query = "select * from Articles where ID = '" . $jsonData['ID'] . "'";
+    $result = $db->query($query);
+    $db->close();
+
+    if ($result->num_rows === 0)
+    {
+        http_response_code(200);
+        $metaData = array();
+        $metaData['state'] = "dumbUser";
+        $metaData['reason'] = "not found";
+        $data['metaData'] = $metaData;
+        echo json_encode($data);
+        die();
+    }
+
+    if ($result->num_rows > 1)
+    {
+        http_response_code(500);
+        die();
+    }
+
+    $neededArticles = $result->fetch_assoc();
+    /*echo print_r($purchase, true);
+    die();*/
+
+    $query = "select * from Articles where householdID = '" . $neededArticles['ID'] . "'";
+    require './dbConnection.php';
+    $result = $db->query($query);
+    $db->close();
+
+    while(($article = $result->fetch_assoc()) !== null)
+    {
+        $query = "select name from Articles where currentAmount < '" . $article['maxAmount'] . "'";
+
+        require './dbConnection.php';
+        $articleResult = $db->query($query);
+        $db->close();
+        $articleName = $articleResult->fetch_assoc();
+        if ($articleName === null)
+        {
+            http_response_code(500);
+            die();
+        }
+
+        $nextArticle = array();
+        $nextArticle['name'] = $articleName['name'];
+        $nextArticle['neededAmount'] = $article['maxAmount'] - $article['currentAmount'];
+
+        $data[$articleName['name']] = $nextArticle;
+    }
+
+    $metaData = array();
+    $metaData['state'] = "success";
+    $data['metaData'] = $metaData;
+
+    http_response_code(200);
+    echo json_encode($data);
+    die();
+?>
