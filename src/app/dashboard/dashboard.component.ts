820d2ad5183eb78dbbcc829aecc17ae02e25f05b
kevinkammleiter <kevinkammleiter@users.noreply.github.com>
2018-01-16 14:44:10 +0100

    #14 added getHouseholdName.php and implemented in dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,11 +9,23 @@ import { Purchase } from '../Purchase';
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.css']
 })
+
+class Household
+{
+    private name: string;
+
+    constructor(name: string)
+    {
+        this.name = name;
+    }
+}
+
 export class DashboardComponent implements OnInit
 {
     private http: Http;
     private router: Router;
     private purchases: Purchase[];
+    private households: Household[];
 
     constructor(http: Http, router: Router)
     {
@@ -32,6 +44,7 @@ export class DashboardComponent implements OnInit
         data['ID'] = JSON.parse(sessionStorage.getItem('currentUser'))['userID'];
         this.http.post('./getUserPurchases.php', JSON.stringify(data)).subscribe(res =>
         {
+            console.log(res.json());
             if(res.status !== 200)
             {
                 return;
@@ -55,6 +68,30 @@ export class DashboardComponent implements OnInit
                     i++;
                 }
                 console.log(this.purchases);
+            }
+        });
+
+        this.http.post('./getHouseholdName.php', JSON.stringify(data)).subscribe(res =>
+        {
+            console.log(res.json());
+            if(res.status !== 200)
+            {
+                return;
+            }
+            if(res.json().metaData.state === "success")
+            {
+                let temp = res.json();
+                delete temp.metaData;
+
+                this.households = new Array((Object.keys(temp)).length);
+                let i = 0;
+                for(let key in temp)
+                {
+                    this.households[i] = new Household(
+                        temp[key].name
+                    );
+                    i++;
+                }
             }
         });
     }
