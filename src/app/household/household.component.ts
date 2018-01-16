56b57f042f301faa1a78ab554b20e732a75225cb
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-01-16 12:52:42 +0100

    #11 do magic

--- src/app/household/household.component.ts
+++ src/app/household/household.component.ts
@@ -60,7 +60,6 @@ export class HouseholdComponent implements OnInit
                     );
                     i++;
                 }
-                console.log(this.purchases)
             }
         });
 
@@ -86,35 +85,35 @@ export class HouseholdComponent implements OnInit
                         temp[key].name,
                         +temp[key].neededAmount
                     );
+                    i++;
+                }
+            }
+        });
+
+        this.http.post('./getStores.php', JSON.stringify(data)).subscribe(res =>
+        {
+            console.log(res.json());
+            if (res.status !== 200)
+            {
+                return;
+            }
+
+            if(res.json().metaData.state === "success")
+            {
+                let temp = res.json();
+                delete temp.metaData;
+
+                this.stores = new Array((Object.keys(temp)).length);
+                let i = 0;
+
+                for(let key in temp)
+                {
+                    this.stores[i] = temp[key].name;
+
                     console.log(key);
                     i++;
                 }
             }
-
-            this.http.post('./getSores', JSON.stringify(data)).subscribe(res =>
-            {
-                console.log(res.json());
-                if (res.status !== 200)
-                {
-                    return;
-                }
-
-                if(res.json().metaData.state === "success")
-                {
-                    let temp = res.json();
-                    delete temp.metaData;
-
-                    this.stores = new Array((Object.keys(temp)).length);
-                    let i = 0;
-
-                    for(let key in temp)
-                    {
-                        this.stores[i] = tmp[key];
-
-                        console.log(key);
-                        i++;
-                    }
-                }
         });
     }
 
