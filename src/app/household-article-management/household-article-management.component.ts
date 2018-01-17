bdc5ec8a1d499ec57158853a97732ec4c970e29b
ewolf34 <ewolf34@users.noreply.github.com>
2018-01-17 14:29:19 +0100

    #13 added add article in typescript and html and created addArticle.php

--- src/app/household-article-management/household-article-management.component.ts
+++ src/app/household-article-management/household-article-management.component.ts
@@ -15,6 +15,13 @@ export class HouseholdArticleManagementComponent implements OnInit
     private router: Router;
     private route: ActivatedRoute;
     private articles: Article[];
+    private newName: string;
+    private newStore: string;
+    private newCurrentAmount: number;
+    private newMinAmount: number;
+    private newMaxAmount: number;
+    private stores: {[key: string]: string;}[];
+
 
     constructor(http: Http, router: Router, route: ActivatedRoute)
     {
@@ -30,7 +37,13 @@ export class HouseholdArticleManagementComponent implements OnInit
             this.router.navigateByUrl('/login');
         }
 
+        getArticles();
+        getStores();
 
+    }
+
+    public getArticles():void
+    {
         let data: {[key: string]: string;} = {};
         data['ID'] = this.route.snapshot.paramMap.get('id');
 
@@ -59,6 +72,64 @@ export class HouseholdArticleManagementComponent implements OnInit
                         temp[key].maxAmount,
                         temp[key].priority
                     );
+                    i++;
+                }
+            }
+        });
+    }
+
+    public addArticle(priority: string, storeIndex: number):void
+    {
+        let data: {[key: string]: string;} = {};
+        data['ID'] = this.route.snapshot.paramMap.get('id');
+        data['name'] = newName;
+        if(storeIndex === -1)
+        {
+            data['storeID'] = newStore;
+        }
+        else
+        {
+            data['storeID'] = stores
+        }
+
+        data['currentAmount'] = newCurrentAmount;
+        data['minAmount'] = newMinAmount;
+        data['maxAmount'] = newMaxAmount;
+        data['priority'] = priority;
+
+        this.http.post('./addHouseholdArticles.php', JSON.stringify(data)).subscribe(res =>
+        {
+            if(res.json().metaData.state === "success")
+            {
+                getArticles();
+                getStores();
+            }
+        });
+
+    }
+
+    public getStores():void
+    {
+        this.http.post('./getStores.php', JSON.stringify(data)).subscribe(res =>
+        {
+            if (res.status !== 200)
+            {
+                return;
+            }
+
+            if(res.json().metaData.state === "success")
+            {
+                let temp = res.json();
+                delete temp.metaData;
+
+                this.stores = new Array((Object.keys(temp)).length);
+                let i = 0;
+
+                for(let key in temp)
+                {
+                    this.stores[i] = {};
+                    this.stores[i]['name'] = temp[key].name;
+                    this.stores[i]['storeID'] = temp[key].storeID;
                     i++;
                 }
             }
