db101a7c6cdd11fc4e7007f49e5f0b44004c585c
Marco13-7 <schaefermawo@gmail.com>
2018-01-18 12:42:40 +0100

    #13 edited deleteArticles.php

--- php/deleteArticle.php
+++ php/deleteArticle.php
@@ -3,4 +3,27 @@
 
     $jsonData = json_decode(file_get_contents('php://input'), true);
     echo print_r($jsonData, true);
+
+    if($jsonData === null)
+    {
+        http_response_code(400);
+        die();
+    }
+    $query = "delete * from Articles where ID = '" . $jsonData . "'";
+    require './dbConnection.php';
+    $result = $db->query($query);
+    $db->close();
+
+    if ($result)
+    {
+        $metaData = array();
+        $metaData['state'] = 'success';
+
+        $data = array();
+        $data['metaData'] = $metaData;
+
+        echo json_encode($data);
+        die();
+    }
+
 ?>
