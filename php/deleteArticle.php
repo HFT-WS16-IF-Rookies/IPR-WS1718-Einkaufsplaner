7d6186c0bf0b6edbd32878d20a7eda03f125db48
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-01-18 14:31:20 +0100

    last fixes

--- php/deleteArticle.php
+++ php/deleteArticle.php
@@ -2,14 +2,14 @@
     require './checkLogin.php';
 
     $jsonData = json_decode(file_get_contents('php://input'), true);
-    echo print_r($jsonData, true);
 
     if($jsonData === null)
     {
         http_response_code(400);
         die();
     }
-    $query = "delete * from Articles where ID = '" . $jsonData . "'";
+
+    $query = "delete from Articles where ID = " . $jsonData;
     require './dbConnection.php';
     $result = $db->query($query);
     $db->close();
