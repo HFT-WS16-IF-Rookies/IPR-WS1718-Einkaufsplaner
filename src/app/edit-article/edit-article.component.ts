ff3cd704fbb1b2cec3ff4b68015c0456cf81a7c9
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-01-18 12:50:53 +0100

    #13 implement edit article partly

--- src/app/edit-article/edit-article.component.ts
+++ src/app/edit-article/edit-article.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/core';
+import { Http } from '@angular/http';
+import { Article } from '../Article';
 
-@Component({
-  selector: 'app-edit-article',
-  templateUrl: './edit-article.component.html',
-  styleUrls: ['./edit-article.component.css']
+@Component
+({
+    selector: 'app-edit-article',
+    templateUrl: './edit-article.component.html',
+    styleUrls: ['./edit-article.component.css']
 })
-export class EditArticleComponent implements OnInit {
+export class EditArticleComponent implements OnInit
+{
+    private http: Http;
+    private router: Router;
+    private route: ActivatedRoute;
 
-  constructor() { }
+    private article: Article;
 
-  ngOnInit() {
-  }
+    constructor(http: Http, router: Router, route: ActivatedRoute)
+    {
+        this.http = http;
+        this.router = router;
+        this.route = route;
+    }
+
+    ngOnInit()
+    {
+        let id = this.route.snapshot.paramMap.get('id');
+        let request: {[key: string]: string} = {};
+        request['articleID'] = id;
+        this.http.post('./getArticle.php', JSON.stringify(request)).subscribe(res =>
+        {
+
+        });
+    }
 
 }
