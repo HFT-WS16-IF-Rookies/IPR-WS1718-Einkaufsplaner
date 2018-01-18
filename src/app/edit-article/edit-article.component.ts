446ed0415604ff74a9ee4f5bbca3069757336230
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-01-18 13:48:00 +0100

    #13 implement edit article partly

--- src/app/edit-article/edit-article.component.ts
+++ src/app/edit-article/edit-article.component.ts
@@ -16,6 +16,7 @@ export class EditArticleComponent implements OnInit
     private route: ActivatedRoute;
 
     private article: Article;
+    private stores: {[key: string]: string;}[];
 
     constructor(http: Http, router: Router, route: ActivatedRoute)
     {
@@ -26,6 +27,8 @@ export class EditArticleComponent implements OnInit
 
     ngOnInit()
     {
+        this.getStores();
+
         let articleID = this.route.snapshot.paramMap.get('articleID');
         let request: {[key: string]: string} = {};
         request['articleID'] = articleID;
@@ -47,6 +50,65 @@ export class EditArticleComponent implements OnInit
                         jsonData.article.priority,
                         jsonData.article.articleID
                     );
+                    break;
+            }
+        });
+    }
+
+    public getStores():void
+    {
+        this.http.post('./getStores.php', '{}').subscribe(res =>
+        {
+            if (res.status !== 200)
+            {
+                return;
+            }
+
+            if(res.json().metaData.state === "success")
+            {
+                let temp = res.json();
+                delete temp.metaData;
+
+                this.stores = new Array((Object.keys(temp)).length);
+                let i = 0;
+
+                for(let key in temp)
+                {
+                    this.stores[i] = {};
+                    this.stores[i]['name'] = temp[key].name;
+                    this.stores[i]['storeID'] = temp[key].storeID;
+                    i++;
+                }
+            }
+        });
+    }
+
+    private saveArticle(priority: string, storeIndex: number):void
+    {
+        let data: {[key: string]: string;} = {};
+        data['articleID'] = ""+this.article.id;
+        data['name'] = this.article.name;
+        data['storeID'] = this.stores[storeIndex]['storeID'];
+
+        data['currentAmount'] = ""+this.article.currentAmount;
+        data['minAmount'] = ""+this.article.minAmount;
+        data['maxAmount'] = ""+this.article.maxAmount;
+        data['priority'] = priority;
+
+        this.http.post('./updateArticle.php', JSON.stringify(data)).subscribe(res =>
+        {
+            let jsonData = res.json();
+            let metaData = jsonData.metaData;
+            delete jsonData.metaData;
+
+            switch (metaData.state)
+            {
+                case 'success':
+                    let householdID = this.route.snapshot.paramMap.get('id');
+                    this.router.navigateByUrl('/household/' + householdID + '/articles');
+                    break;
+
+                case 'error':
                     break;
             }
         });
